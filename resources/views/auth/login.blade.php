6648c8f3471b6a3ebecbf7fd0975e17001760b79
Moch Surya <74476219+msuryaa@users.noreply.github.com>
2024-06-05 00:45:00 +0700

    Login
    ganti Warna

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,64 +29,61 @@
                 {{-- <nav class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0 mx-4">
                     <div class="container-fluid">
                         <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="{{route ('login')}}">
-                            <!-- Argon Dashboard 2 -->
-                            <img src="{{asset ('admin/assets/img/logo (1).png')}}" alt="">
-                        </a>
-                        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon mt-2">
-                                <span class="navbar-toggler-bar bar1"></span>
-                                <span class="navbar-toggler-bar bar2"></span>
-                                <span class="navbar-toggler-bar bar3"></span>
-                            </span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navigation">
-                            <ul class="navbar-nav mx-6">
-                                <li class="nav-item mx-3">
-                                    <a class="nav-link d-flex align-items-center me-2 active" aria-current="page"
-                                        href="../pages/dashboard.html">
-                                        <!-- <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i> -->
-                                        Dashboard
-                                    </a>
-                                </li>
-                                <li class="nav-item mx-3">
-                                    <a class="nav-link me-2" href="../pages/profile.html">
-                                        <!-- <i class="fa fa-user opacity-6 text-dark me-1"></i> -->
-                                        Product
-                                    </a>
-                                </li>
-                                <li class="nav-item mx-3">
-                                    <a class="nav-link me-2" href="../pages/sign-up.html">
-                                        <!-- <i class="fas fa-user-circle opacity-6 text-dark me-1"></i> -->
-                                        Category
-                                    </a>
-                                </li>
-                                <li class="nav-item mx-3">
-                                    <a class="nav-link me-2" href="../pages/sign-in.html">
-                                        <!-- <i class="fas fa-key opacity-6 text-dark me-1"></i> -->
-                                        Orders
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link me-2" href="../pages/sign-in.html">
-                                        <!-- <i class="fas fa-key opacity-6 text-dark me-1"></i> -->
-                                        Users
-                                    </a>
-                                </li>
-                            </ul>
-                            <!-- <ul class="navbar-nav d-lg-block d-none">
+                <!-- Argon Dashboard 2 -->
+                <img src="{{asset ('admin/assets/img/logo (1).png')}}" alt="">
+                </a>
+                <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon mt-2">
+                        <span class="navbar-toggler-bar bar1"></span>
+                        <span class="navbar-toggler-bar bar2"></span>
+                        <span class="navbar-toggler-bar bar3"></span>
+                    </span>
+                </button>
+                <div class="collapse navbar-collapse" id="navigation">
+                    <ul class="navbar-nav mx-6">
+                        <li class="nav-item mx-3">
+                            <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="../pages/dashboard.html">
+                                <!-- <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i> -->
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link me-2" href="../pages/profile.html">
+                                <!-- <i class="fa fa-user opacity-6 text-dark me-1"></i> -->
+                                Product
+                            </a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link me-2" href="../pages/sign-up.html">
+                                <!-- <i class="fas fa-user-circle opacity-6 text-dark me-1"></i> -->
+                                Category
+                            </a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link me-2" href="../pages/sign-in.html">
+                                <!-- <i class="fas fa-key opacity-6 text-dark me-1"></i> -->
+                                Orders
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link me-2" href="../pages/sign-in.html">
+                                <!-- <i class="fas fa-key opacity-6 text-dark me-1"></i> -->
+                                Users
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- <ul class="navbar-nav d-lg-block d-none">
                                 <li class="nav-item">
                                     <a href="https://www.creative-tim.com/product/argon-dashboard"
                                         class="btn btn-sm mb-0 me-1 btn-primary">Free Download</a>
                                 </li>
                             </ul> -->
-                        </div>
-                    </div>
-                </nav> --}}
-                <!-- End Navbar -->
+                </div>
             </div>
+            </nav> --}}
+            <!-- End Navbar -->
         </div>
+    </div>
     </div>
     <main class="main-content  mt-0">
         <section>
@@ -96,53 +93,43 @@
                         <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto mt-7">
                             <div class="card card-plain">
                                 <div class="card-header pb-0 text-start px-0">
-                                    <h4 class="font-weight-bolder">Sign In</h4>
-                                    <p class="mb-0">Enter your email and password to sign in</p>
+                                    <h4 class="font-weight-bolder">Masuk</h4>
+                                    <p class="mb-0">Masukkan email dan kata sandi anda untuk melanjutkan</p>
                                 </div>
                                 <div class="card-body px-0">
                                     <form method="POST" action="{{ route('login') }}">
                                         @csrf
                                         <div class="mb-3">
-                                            <input type="email" id="email" for="email" name="email"
-                                                class="form-control form-control-lg" placeholder="Email"
-                                                aria-label="Email" required autofocus autocomplete="username">
+                                            <input type="email" id="email" for="email" name="email" class="form-control form-control-lg" placeholder="Email" aria-label="Email" required autofocus autocomplete="username">
                                         </div>
                                         <div class="mb-3">
-                                            <input for="password" id="password" class="form-control form-control-lg"
-                                                placeholder="Password" aria-label="Password" type="password"
-                                                name="password" required autocomplete="current-password">
+                                            <input for="password" id="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password" type="password" name="password" required autocomplete="current-password">
                                             <x-input-error :messages="$errors->get('email')" class="mt-2" />
                                         </div>
                                         <div class="form-check form-switch">
-                                            <input class="form-check-input" type="checkbox" id="remember_me"
-                                                name="remember">
-                                            <label class="form-check-label" for="remember_me">Remember me</label>
+                                            <input class="form-check-input" type="checkbox" id="remember_me" name="remember">
+                                            <label class="form-check-label" for="remember_me">Ingat akun saya</label>
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-lg btn-lg w-100 mt-4 mb-0"
-                                                style="background-color: #24263D; color: #fff;">Sign
+                                            <button type="submit" class="btn btn-lg btn-lg w-100 mt-4 mb-0" style="background-color: #EF0003; color: #fff;">Sign
                                                 in</button>
                                         </div>
                                     </form>
                                 </div>
                                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
                                     <p class="mb-1 text-sm mx-auto">
-                                        <a href="{{ route('password.request') }}" class="font-weight-bold"
-                                            style="color: #24263D">
-                                            Forgot Password?</a>
+                                        <a href="{{ route('password.request') }}" class="font-weight-bold" style="color: #EF0003">
+                                            Lupa kata sandi?</a>
                                     </p>
                                     <p class="mb-4 text-sm mx-auto">
-                                        Don't have an account?
-                                        <a href="{{ route('register') }}" class="font-weight-bold"
-                                            style="color: #24263D">Sign up</a>
+                                        Belum punya akun ?
+                                        <a href="{{ route('register') }}" class="font-weight-bold" style="color: #EF0003">Daftar Sekarang</a>
                                     </p>
                                 </div>
                             </div>
                         </div>
-                        <div
-                            class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-                            <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
-                                style="background-image: url('{{ asset('admin/assets/img/login-bg.png') }}');
+                        <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+                            <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('{{ asset('admin/assets/img/login-bg.png') }}');
           background-size: cover;">
                                 <!-- <span class="mask bg-gradient-primary opacity-3"></span> -->
                                 <span class="mask login-bg"></span>

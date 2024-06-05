75ec3296edfc91f023da68e65bbc502e2a5e8322
jtrintern <dzakiaf19@gmail.com>
2024-06-05 11:03:55 +0700

    tes payload

--- app/Http/Controllers/API/MidtransController.php
+++ app/Http/Controllers/API/MidtransController.php
@@ -10,70 +10,74 @@ use Midtrans\Notification;
 
 class MidtransController extends Controller
 {
-    public function callback()
+    public function callback(Request $request)
     {
-        //kofigurasi midtrans
-        Config::$serverKey = config('services.midtrans.serverKey');
-        Config::$isProduction = config('services.midtrans.isProduction');
-        Config::$isSanitized = config('services.midtrans.isSanitized');
-        Config::$is3ds = config('services.midtrans.is3ds');
+        $payload = $request->all();
 
-        //instance midtrans notif
-        $notification = new Notification();
+        dd($payload);
 
-        //assign variable
-        $status = $notification->transaction_status;
-        $type = $notification->payment_type;
-        $fraud = $notification->fraud_status;
-        $order_id = $notification->order_id;
+        // //kofigurasi midtrans
+        // Config::$serverKey = config('services.midtrans.serverKey');
+        // Config::$isProduction = config('services.midtrans.isProduction');
+        // Config::$isSanitized = config('services.midtrans.isSanitized');
+        // Config::$is3ds = config('services.midtrans.is3ds');
 
-        //get transaction id
-        $order = explode('-', $order_id);
+        // //instance midtrans notif
+        // $notification = new Notification();
 
-        //cari transaksi
-        $transaction = Order::findOrFail($order[1]);
+        // //assign variable
+        // $status = $notification->transaction_status;
+        // $type = $notification->payment_type;
+        // $fraud = $notification->fraud_status;
+        // $order_id = $notification->order_id;
 
-        //handle notif
-        if ($status == 'capture') {
-            if ($type == 'gopay') {
-                if ($fraud == 'challange') {
-                    $transaction->status = 'PENDING';
-                }else{
-                    $transaction->status = 'SUCCESS';
-                }
-            }else if ($type == 'bank_transfer') {
-                if ($fraud == 'challange') {
-                    $transaction->status = 'PENDING';
-                }else{
-                    $transaction->status = 'SUCCESS';
-                }
-            }else if ($type == 'bca_va') {
-                if ($fraud == 'challange') {
-                    $transaction->status = 'PENDING';
-                }else{
-                    $transaction->status = 'SUCCESS';
-                }
-            }
-        }else if($status == 'settlement'){
-            $transaction->status = 'SUCCESS';
-        }else if($status == 'pending'){
-            $transaction->status = 'PENDING';
-        }else if($status == 'deny'){
-            $transaction->status = 'PENDING';
-        }else if($status == 'expired'){
-            $transaction->status = 'CANCELLED';
-        }else if($status == 'cancel'){
-            $transaction->status = 'PENDING';
-        }
+        // //get transaction id
+        // $order = explode('-', $order_id);
 
-        //simpan transaksi
-        $transaction->save();
+        // //cari transaksi
+        // $transaction = Order::findOrFail($order[1]);
 
-        return response()->json([
-            'meta' => [
-                'code' => 200,
-                'message' => 'Midtrans Notification Success!'
-            ],
-        ]);
+        // //handle notif
+        // if ($status == 'capture') {
+        //     if ($type == 'gopay') {
+        //         if ($fraud == 'challange') {
+        //             $transaction->status = 'PENDING';
+        //         }else{
+        //             $transaction->status = 'SUCCESS';
+        //         }
+        //     }else if ($type == 'bank_transfer') {
+        //         if ($fraud == 'challange') {
+        //             $transaction->status = 'PENDING';
+        //         }else{
+        //             $transaction->status = 'SUCCESS';
+        //         }
+        //     }else if ($type == 'bca_va') {
+        //         if ($fraud == 'challange') {
+        //             $transaction->status = 'PENDING';
+        //         }else{
+        //             $transaction->status = 'SUCCESS';
+        //         }
+        //     }
+        // }else if($status == 'settlement'){
+        //     $transaction->status = 'SUCCESS';
+        // }else if($status == 'pending'){
+        //     $transaction->status = 'PENDING';
+        // }else if($status == 'deny'){
+        //     $transaction->status = 'PENDING';
+        // }else if($status == 'expired'){
+        //     $transaction->status = 'CANCELLED';
+        // }else if($status == 'cancel'){
+        //     $transaction->status = 'PENDING';
+        // }
+
+        // //simpan transaksi
+        // $transaction->save();
+
+        // return response()->json([
+        //     'meta' => [
+        //         'code' => 200,
+        //         'message' => 'Midtrans Notification Success!'
+        //     ],
+        // ]);
     }
 }

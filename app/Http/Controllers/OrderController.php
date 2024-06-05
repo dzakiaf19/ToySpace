2a923569e96db6da2d192f56c73f7939de96d5a1
jtrintern <dzakiaf19@gmail.com>
2024-06-05 18:48:12 +0700

    aaa

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -76,7 +76,7 @@ class OrderController extends Controller
                     'phone' => $address->phone,
                     'courier' => "JNE (" . $ongkir['nama'] . ")",
                     'total_price' => $total + $ongkir['harga'],
-                    'ongkos_kirim' => $ongkir['harga'],
+                    'ongkos_price' => $ongkir['harga'],
                 ]);
 
                 //list transaksi

3e83919a45bd583d81d79f796d378d791791f633
jtrintern <dzakiaf19@gmail.com>
2024-06-12 15:22:41 +0700

    Update edit_profile.blade.php

--- resources/views/toyspace/page/edit_profile.blade.php
+++ resources/views/toyspace/page/edit_profile.blade.php
@@ -19,16 +19,16 @@
                                         <label for="namadepan" class="form-label">Nama Depan</label>
                                         <input id="namadepan" name="firstName" class="form-control" type="text"
                                             value="{{ $user->firstName }}" aria-label="default input example">
-                                        <x-input-error :messages="$errors->get('firstName')" class="mt-2" />
                                     </div>
+                                    <x-input-error :messages="$errors->get('firstName')" class="mt-2" />
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-outline mb-4">
                                         <label for="namabelakang" class="form-label">Nama Belakang</label>
                                         <input id="namabelakang" name="lastName" class="form-control" type="text"
                                             value="{{ $user->lastName }}" aria-label="default input example">
-                                        <x-input-error :messages="$errors->get('lastName')" class="mt-2" />
                                     </div>
+                                    <x-input-error :messages="$errors->get('lastName')" class="mt-2" />
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -37,8 +37,8 @@
                                         <label for="birthdate" class="form-label">Tanggal Lahir</label>
                                         <input id="birthdate" name="birthdate" type="date" class="form-control"
                                             value="{{ $user->birthdate }}">
-                                        <x-input-error :messages="$errors->get('birthdate')" class="mt-2" />
                                     </div>
+                                    <x-input-error :messages="$errors->get('birthdate')" class="mt-2" />
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-outline mb-4">
@@ -48,8 +48,8 @@
                                                 style="background-color: #e9ecef;">+62</span>
                                             <input type="text" name="phone" class="form-control" maxlength="11"
                                                 value="{{ $user->phone }}">
-                                            <x-input-error :messages="$errors->get('phone')" class="mt-2" />
                                         </div>
+                                        <x-input-error :messages="$errors->get('phone')" class="mt-2" />
                                     </div>
                                 </div>
                             </div>
